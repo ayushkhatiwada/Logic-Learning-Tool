b264491e64fc4722a7b7430076dfc90b42b42429
Jan Pytel <jas.pytel@gmail.com>
2023-03-16 18:16:18 +0000

    Instructions page

--- src/pages/Instructions.js
+++ src/pages/Instructions.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import instructions1 from '../img/instructions1.png'
+import instructions2 from '../img/instructions2.png'
+import styles from '../styles/Instructions.module.scss'
 
 export default function Instructions() {
   return (
-    <div>Instructions</div>
+    <div className={styles.container}>
+      <p className={styles.instructionText}>Instructions</p>
+      
+      <img src={instructions1} alt="instructions1" />
+      <img src={instructions2} alt="instructions2" />
+    </div>
   )
 }
