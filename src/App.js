1d0616ce5b1ec8c9b48f88d026dbc45038ce8a9e
Jan Pytel <jas.pytel@gmail.com>
2023-03-16 18:06:39 +0000

    Home page and working routing system

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Game from './pages/Game';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Game from "./pages/Game"
+import Settings from "./pages/Settings"
+import Instructions from "./pages/Instructions"
+import Credits from "./pages/Credits"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Game />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/instructions" element={<Instructions />} />
+        <Route path="/credits" element={<Credits />} />
         <Route path="*" element={<h1>404 Page not found</h1>} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import backgroundImg from '../img/home-background.png'
+import gameLogoImg from '../img/game-logo.png'
+import styles from "../styles/Home.module.scss"
+
+export default function Home() {
+  return (
+    <div 
+      className={styles.background}
+      style={{ backgroundImage: `url(${backgroundImg})` }}
+    >
+      <div className={styles.container}>
+        <img className={styles.gameLogo} src={gameLogoImg}></img>
+        <div className={styles.buttons}>
+          <Link to="/game"><div className={styles.button}>Start Game</div></Link>
+          <Link to="/settings"><div className={styles.button}>Settings</div></Link>
+          <Link to="/instructions"><div className={styles.button}>Instructions</div></Link>
+          <Link to="/credits"><div className={styles.button}>Credits</div></Link>
+        </div>
+      </div>
+    </div>
+  )
+}
