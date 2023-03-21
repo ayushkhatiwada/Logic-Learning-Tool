a179055249177328c318176699ccb29c32728807
mikoto-5813 <122571538+mikoto-5813@users.noreply.github.com>
2023-03-21 21:43:23 +0000

    Update Instructions.js

--- src/pages/Instructions.js
+++ src/pages/Instructions.js
@@ -2,12 +2,17 @@ import React from 'react'
 import instructions1 from '../img/instructions1.png'
 import instructions2 from '../img/instructions2.png'
 import styles from '../styles/Instructions.module.scss'
+import gohome from '../img/homesymbol.png' //gohomo.png <- which do you think is bette??
+import { Link } from 'react-router-dom'
 
 export default function Instructions() {
   return (
     <div className={styles.container}>
       <p className={styles.instructionText}>Instructions</p>
-      
+        <Link to="/" style={{position: "absolute", top : 50, left: 256 }}>
+          <img src={gohome} style={{width: "30px"}}/>
+        </Link>
+
       <img src={instructions1} alt="instructions1" />
       <img src={instructions2} alt="instructions2" />
     </div>
