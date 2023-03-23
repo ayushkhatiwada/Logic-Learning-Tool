59b5c83c3ee457c620391bb99840a8fdd8a65339
Jan Pytel <jas.pytel@gmail.com>
2023-03-23 13:57:58 +0000

    Stylesheet for the HomeButton

--- src/components/shared/homeButton/HomeButton.js
+++ src/components/shared/homeButton/HomeButton.js
@@ -1,10 +1,13 @@
 import homeIcon from "../../../img/homesymbol.png"
 import { Link } from "react-router-dom"
+import styles from "./HomeButton.module.scss"
 
 export default function HomeButton() {
   return (
-    <Link to="/" style={{ position: "absolute", top: 10, left: 10 }}>
-      <img src={homeIcon} style={{ width: "30px" }} />
-    </Link>
+    <div className={styles.homeButton}>
+      <Link to="/">
+        <img src={homeIcon} />
+      </Link>
+    </div>
   )
 }
